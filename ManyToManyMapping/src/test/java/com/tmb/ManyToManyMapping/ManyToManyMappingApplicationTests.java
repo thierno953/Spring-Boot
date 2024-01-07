50646b71e0fb6754d7a54954ecedfff17e7ceed5
thierno953 <77080504+thierno953@users.noreply.github.com>
2024-01-07 16:18:44 +0100

    JPA Many-to-Many

---
+++ ManyToManyMapping/src/test/java/com/tmb/ManyToManyMapping/ManyToManyMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmb.ManyToManyMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
