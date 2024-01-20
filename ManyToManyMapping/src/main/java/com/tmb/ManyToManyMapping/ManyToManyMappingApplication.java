b52eaea394e6e3fc30d5b196edc06d8b1c54d61c
thierno953 <77080504+thierno953@users.noreply.github.com>
2024-01-20 14:13:29 +0100

    Spring Data JPA - MySQL, Relationship - Has-A, Many-to-Many Mapping

---
+++ ManyToManyMapping/src/main/java/com/tmb/ManyToManyMapping/ManyToManyMappingApplication.java
@@ -0,0 +1,13 @@
+package com.tmb.ManyToManyMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyToManyMappingApplication.class, args);
+	}
+
+}
