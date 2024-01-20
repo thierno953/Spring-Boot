34deb859f9a9009fbf865230fe93328d84d56a46
thierno953 <77080504+thierno953@users.noreply.github.com>
2024-01-20 10:40:30 +0100

    Spring Data JPA - MySQL Relationship One-to-One Mapping

---
+++ OneToOneMapping/src/test/java/com/tmb/OneToOneMapping/OneToOneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmb.OneToOneMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
